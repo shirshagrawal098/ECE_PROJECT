62f21881276f52613d8334a3db81b878ad37d49c
chowdeswari15 <chowdeswari29@gmail.com>
2024-10-09 11:25:47 +0530

    Update temperature_converter.c

--- 3_implementation/src/temperature_converter.c
+++ 3_implementation/src/temperature_converter.c
@@ -1,1 +1,10 @@
 
+#include "temperature_converter.h"
+
+float celsius_to_fahrenheit(float celsius) {
+    return (celsius * 9.0 / 5.0) + 32.0;
+}
+
+float fahrenheit_to_celsius(float fahrenheit) {
+    return (fahrenheit - 32.0) * 5.0 / 9.0;
+}

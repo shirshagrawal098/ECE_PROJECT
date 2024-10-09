3bf32e20233ebb4f775e35633b96dd58cda01b3c
chowdeswari15 <chowdeswari29@gmail.com>
2024-10-09 11:21:30 +0530

    Create temperature_converter.h

---
+++ 3_implementation/inc/temperature_converter.h
@@ -0,0 +1,8 @@
+
+#ifndef TEMPERATURE_CONVERTER_H
+#define TEMPERATURE_CONVERTER_H
+
+float celsius_to_fahrenheit(float celsius);
+float fahrenheit_to_celsius(float fahrenheit);
+
+#endif // TEMPERATURE_CONVERTER_H
